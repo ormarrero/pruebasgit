71d1a53ac7348a2a62af55d937c8c0ecccb209f7
ormarrero <104150268+ormarrero@users.noreply.github.com>
2023-01-21 17:07:40 +0100

    clase employee

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -5,4 +5,36 @@ public class Employee {
     private Integer age;
     private Double salary;
 
+    public Employee() {
+    }
+
+    public Employee(String name, Integer age, Double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
